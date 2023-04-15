1eb56e9de1a0771abed458cac646dfe80fb57936
coldwindx <15653913658@163.com>
2023-04-15 19:50:10 +0800

    01.intro

--- demo.py
+++ demo.py
@@ -4,6 +4,11 @@
 #alt Pipeline inputs, program, results
 import fastbook
 from fastai.vision.all import *
-import graphviz
-s = "program[shape=box3d width=1 height=0.7] inputs->program->results"
-graphviz.Source('digraph G{ rankdir="LR"' + s + '; }')
+path = untar_data(URLs.CAMVID_TINY)
+dls = SegmentationDataLoaders.from_label_func(
+    path, bs=8, fnames=get_image_files(path/'images'),
+    label_func=lambda o: path/'labels'/f'{o.stem}_P{o.suffix}',
+    codes=np.loadtxt(path/'codes.txt', dtype=str)
+)
+learn = unet_learner(dls, resnet34)
+learn.fine_tune(8)
